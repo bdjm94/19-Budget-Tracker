39856eb732450afd3884384e65a2aa7283006fd9
bdjm <brendandjmoore@gmail.com>
2021-07-13 19:23:49 +1000

    function to install

--- public/service-worker.js
+++ public/service-worker.js
@@ -11,3 +11,15 @@ const FILES_TO_CACHE = [
     "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
     "https://cdn.jsdelivr.net/npm/chart.js@2.8.0"
 ];
+
+const STATIC_CACHE = "static-cache-v1";
+const RUNTIME_CACHE = "runtime-cache";
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches
+        .open(STATIC_CACHE)
+        .then((cache) => cache.addAll(FILES_TO_CACHE))
+        .then(self.skipWaiting())
+    );
+  });
