60a332059578ede7678f7fd0a2ef62a1a28a7a8f
bdjm <brendandjmoore@gmail.com>
2021-07-13 18:48:07 +1000

    assign the current store to a variable

--- public/db.js
+++ public/db.js
@@ -46,6 +46,8 @@ function checkDatabase() {
             .then((res) => {
                 if(res.length !== 0) {
                     transaction = db.transaction(['BudgetStore'], 'readwrite');
+
+                    const currentStore = transaction.objectStore('BudgetStore');
                 }
             })
         }
